c6e18ca5f6058b8ca81f54750438d4a5ddf58d75
dolwijit13 <dol51890@gmail.com>
2022-10-31 21:36:44 +0700

    Turbo frame, Turbo stream and Stimulus

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :post
+  broadcasts_to :post
+  # after_create_commit -> { broadcast_append_to post }
+  # after_destroy_commit -> { broadcast_remove_to post }
+  # after_update_commit -> { broadcast_replace_to post }
 end
